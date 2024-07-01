7c02ae56c4a18c82aaeb1c1739efe7ad1183ac71
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-01 15:23:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- Add csss file -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Add fontawesome icons -->
+    <link rel="icon" href="./img/logo.png">
+
+    <!-- boostrap 5-->
+
+    <title>SMS</title>
+<!-- tailwind-->
+<script src="https://cdn.tailwindcss.com"></script>
+   
+       
+</head>
+
+<body>
+
+    <?php include('./common/Nav.php'); ?>
+
+
+
+
+
+
+
+
+<?php include('./common/fotter.php'); ?>
+
+</body>
+
+</html>
